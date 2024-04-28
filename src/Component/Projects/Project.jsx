6110014aea8ee777a31dcb0a0a0fd2b925c7ef11
nitroEef <mikebodetelli@gmail.com>
2024-04-28 19:25:40 +0100

    update

--- src/Component/Projects/Project.jsx
+++ src/Component/Projects/Project.jsx
@@ -4,23 +4,25 @@ import './Project.css';
 
 const Project = () => {
   return (
-    <div>
-      <div className="container">
+    <>
+    
+      <h3 className='Project'> My Projects </h3>
+    <div className="container">
 
-        <h3 className='Project'> My Projects </h3>
-        <div className='onebox'></div>
+        <div className='onebox'>
 
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
-        <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
+          <div className="box">ajaj</div>
 
+        </div>
       </div>
-    </div>
+      </>
   )
 }
 
