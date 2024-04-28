0ce5d46a482f82f9f2de212ef2b30e421654bdc1
nitroEef <mikebodetelli@gmail.com>
2024-04-28 19:02:08 +0100

    update

--- src/Component/Projects/Project.jsx
+++ src/Component/Projects/Project.jsx
@@ -6,6 +6,10 @@ const Project = () => {
   return (
     <div>
       <div className="container">
+
+        <h3 className='Project'> My Projects </h3>
+        <div className='onebox'></div>
+
         <div className="box">ajaj</div>
         <div className="box">ajaj</div>
         <div className="box">ajaj</div>
