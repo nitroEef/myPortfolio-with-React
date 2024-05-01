6ec51fc0e067a6f0c137b9574b58a8bb17d9b423
nitroEef <mikebodetelli@gmail.com>
2024-05-01 11:38:02 +0100

    updste

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,41 +1,13 @@
-
-      
-import React, { useState } from 'react';
-import './Contact.css';
+import React , {useRef }from 'react'
 
 const Contact = () => {
-    
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Phone:', phone);
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
-      </label>
-      <label>
-        Phone:
-        <input type="tel" value={phone} onChange={(event) => setPhone(event.target.value)} />
-      </label>
-      <button type="submit">Submit</button>
-    </form>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
+export default Contact
 
-    
-export default Contact;
 
--- src/Component/HeroSection/HeroSection.jsx
+++ src/Component/HeroSection/HeroSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './HeroSection.css';
+import { Fa4, FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa6';
 
 const HeroSection = () => {
   
@@ -8,18 +9,21 @@ const HeroSection = () => {
    
 
     <div className='imageAndText'>
-        <div className='threeTexts'>
+        <d iv className='threeTexts'>
    
     <h2 className='intro'>Hello, I am!</h2>
     <h3 className='introTwo'>Ogbeni NitroEef</h3>
     <h4 className='introThree'>a professional web developer and UI/UX designer</h4>
 <div className='buttonguys'>
-    <button className=' btn resume'> Resume</button>
-    <button className='btn myWork'> Contact Me</button>
+    <button className=' btn resume'>  My Resume</button>
+    {/* <button className='btn myWork'> Contact Me</button> */}
+    <div className='myIcons'>
+    <FaGithub/> <FaLinkedin/> <FaFacebook/> <FaTwitter/> <FaInstagram/>
+    </div>
  </div>
 
 
-    </div>
+    </d>
 
 <div className='quotebox'>
   <p className='thequote'>'''Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi exercitationem nisi excepturi ratione mollitia beatae enim odit sint possimus esse, 
