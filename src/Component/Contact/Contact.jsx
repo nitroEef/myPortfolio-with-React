717eb2ef39dbc7e0460c2b884962a85baac8615d
nitroEef <mikebodetelli@gmail.com>
2024-05-02 09:56:40 +0100

    update

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,45 +1,77 @@
+// import React, { useRef } from 'react';
+// import emailjs from '@emailjs/browser';
+// import './Contact.css';
+
+//  const Contact = () => {
+//   const form = useRef();
+
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+
+//     emailjs
+//       .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+//         publicKey: 'YOUR_PUBLIC_KEY',
+//       })
+//       .then(
+//         () => {
+//           console.log('your message is sent!');
+//         },
+//         (error) => {
+//           console.log('oshi lo te...', error.text);
+//         },
+//       );
+//   };
+
+//   return (
+//     <div className='contacted'>
+//       <h3 className='send'>Contact Me</h3> 
+//     <form ref={form} onSubmit={sendEmail}>
+//       <input type="text" name="user_name" placeholder='Name' />
+
+//       <input type="email" name="user_email" placeholder='Email'  />
+
+      
+//       <input type="number" name="user_number" placeholder='Phone Number'  />
+
+
+//       <textarea name="message" placeholder='Send your Message' />
+
+//       <button className='bttn'>Send Message</button>
+
+//     </form>
+//     </div>
+//   );
+// };
+// export default Contact
+
+
 import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
-import './Contact.css';
+import './Contact.css'
 
- const Contact = () => {
-  const form = useRef();
+const ContactForm = () => {
+  const form = useRef(null);
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const sendEmail = (event) => {
+    event.preventDefault();
+    // Code to send email goes here
 
-    emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
-      .then(
-        () => {
-          console.log('your message is sent!');
-        },
-        (error) => {
-          console.log('oshi lo te...', error.text);
-        },
-      );
+    // Reset the form after submission to clear the input fields
+    form.current.reset();
   };
 
   return (
     <div className='contacted'>
       <h3 className='send'>Contact Me</h3> 
-    <form ref={form} onSubmit={sendEmail}>
-      <input type="text" name="user_name" placeholder='Name' />
-
-      <input type="email" name="user_email" placeholder='Email'  />
-
-      
-      <input type="number" name="user_number" placeholder='Phone Number'  />
-
-
-      <textarea name="message" placeholder='Send your Message' />
-
-      <button className='bttn'>Send Message</button>
-
-    </form>
+      <form ref={form} onSubmit={sendEmail}>
+        <input type="text" name="user_name" placeholder='Name' />
+        <input type="email" name="user_email" placeholder='Email' />
+        <input type="number" name="user_number" placeholder='Phone Number' />
+        <textarea name="message" placeholder='Send your Message' />
+        <button className='bttn'>Send Message</button>
+      </form>
     </div>
   );
 };
-export default Contact
+
+export default ContactForm;
+
