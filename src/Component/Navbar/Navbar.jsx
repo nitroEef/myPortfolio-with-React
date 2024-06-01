b4a857fb3c190b6e15b16144332a51d929441d88
nitroEef <mikebodetelli@gmail.com>
2024-06-01 15:57:07 +0100

    update

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { FaTimes, FaBars } from "react-icons/fa";
 import "./Navbar.css";
 import { hy } from '../../assets';
 
-NDODONDKOEN
+NDODONDK
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
