721c2029f9961569b7337c24902a92a9c856789d
nitroEef <mikebodetelli@gmail.com>
2024-06-08 22:57:12 +0100

    update

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -11,7 +11,7 @@ import './Contact.css';
     alert('Message sent successfully!, Eef will surely get back to you soon');
   
     
-uiwuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+uiwuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
 
     emailjs
       .sendForm('service_if797uo', 'template_1m8u6fr', form.current, {
