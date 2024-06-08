add8eb8ee98201ae213678ccf794ae4f4c7c3b85
nitroEef <mikebodetelli@gmail.com>
2024-06-08 22:50:40 +0100

    update

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -13,7 +13,7 @@ import './Contact.css';
     
 jen    klnnkknknkn
 ;,llmlmlmlmlm
-,cd b,jjjjjjjjjjjjjjjkbjkbbbbbbbbbbbbbbbbbbbbbbbbbbbrrrrrrrrr
+,cd b,jjjjjjjjjjjjjjjkbjkbbbbbbbbbbbbbbbbbbbbbbbb
 
     emailjs
       .sendForm('service_if797uo', 'template_1m8u6fr', form.current, {
